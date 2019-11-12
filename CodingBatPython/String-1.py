a549c6c5b75b135b9a45376cb2b831d4987c8ab5
Jeremy <Jeremy.yao.info@gmail.com>
2019-11-12 01:48:48 -0500

    finish String-1

--- CodingBatPython/String-1.py
+++ CodingBatPython/String-1.py
@@ -19,3 +19,24 @@ def first_two(str):
   if len(str) > 2:
     result = str[:2]
   return result 
+
+def first_half(str):
+  return str[:len(str)/2]
+
+def without_end(str):
+  return str[1:len(str)-1]
+
+def combo_string(a, b):
+  short = a
+  long = b
+  if len(a) > len(b):
+    short = b
+    long = a
+    
+  return  short+long+short
+
+def non_start(a, b):
+  return a[1:] + b[1:]
+
+def left2(str):
+  return str[2:] + str[:2]
